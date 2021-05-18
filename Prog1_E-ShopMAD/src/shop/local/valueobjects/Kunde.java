678b346348b4986bdd29297fde015a8abb2738b1
Anna <67149628+awirbel327@users.noreply.github.com>
2021-05-18 10:31:11 +0200

    Kunden getter/setter usw

--- Prog1_E-ShopMAD/src/shop/local/valueobjects/Kunde.java
+++ Prog1_E-ShopMAD/src/shop/local/valueobjects/Kunde.java
@@ -2,16 +2,62 @@ package shop.local.valueobjects;
 
 public class Kunde {
 
-	
-	/**
-	 * Vorname
-	 * nachname
-	 * adresse
-	 * benutzername
-	 * passwort
-	 * 
-	 * 
-	 */
-	
-	
+private int kundenNr;
+
+private String name;
+private String strasse = " ";
+private String hausNr = "";
+private String plz = " ";
+private String ort = " ";
+
+public Kunde(int nr, String name) {
+	kundenNr = nr;
+	this.name = name;
+	}
+
+// getter und setter usw Kunden-Eigenschaften
+public int getKundenNr() {
+	return kundenNr;
+}
+
+public String getName() {
+	return name;
+	}
+
+public void setName(String name) {
+	this.name = name;
+	}
+
+public String getPlz() {
+	return plz;
+	}
+
+public void setPlz(String plz) {
+	this.plz = plz;
+	}
+
+public String getStrasse() {
+	return strasse;
+	}
+
+public void setStrasse(String strasse) {
+	this.strasse = strasse;
+	}
+
+public String getHausNr() {
+	return hausNr;
+	}
+
+public void setHausNr(String hausNr) {
+	this.hausNr = hausNr;
+	}
+
+public String getOrt() {
+	return ort;
+	}
+
+public void setOrt(String ort) {
+	this.ort = ort;
+	}
+
 }
