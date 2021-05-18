c5a72a94c34007ffb128ddd2ffad1796348a0beb
Anna <67149628+awirbel327@users.noreply.github.com>
2021-05-18 10:57:26 +0200

    String Warenkorb/Kundenpasswort und Username + getter und setter

--- Prog1_E-ShopMAD/src/shop/local/valueobjects/Kunde.java
+++ Prog1_E-ShopMAD/src/shop/local/valueobjects/Kunde.java
@@ -9,10 +9,15 @@ private String strasse = " ";
 private String hausNr = "";
 private String plz = " ";
 private String ort = " ";
+private String kUsername;	//getter und setter ? getter einfach gemacht
+private String kPasswort;	//getter und setter ? getter einfach gemacht
+private Warenkorb warenkorb;
 
 public Kunde(int nr, String name) {
 	kundenNr = nr;
 	this.name = name;
+	warenkorb = new Warenkorb();
+	//mitarbeiter = false;
 	}
 
 // getter und setter usw Kunden-Eigenschaften
@@ -58,6 +63,19 @@ public String getOrt() {
 
 public void setOrt(String ort) {
 	this.ort = ort;
+	}
+
+public String getKUsername() {
+	return kUsername;
+	}
+
+public String getKPasswort() {
+	return kPasswort;
+	}
+
+
+public Warenkorb getWk() {
+	return warenkorb;
 	}
 
 }
