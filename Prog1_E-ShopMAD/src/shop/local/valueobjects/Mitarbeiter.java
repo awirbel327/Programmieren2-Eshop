690b05ac9f37d70c7ef6ca8847d6317285ee06ef
Anna <67149628+awirbel327@users.noreply.github.com>
2021-05-18 10:36:48 +0200

    Getter/Setter usw Mitarbeiter

--- Prog1_E-ShopMAD/src/shop/local/valueobjects/Mitarbeiter.java
+++ Prog1_E-ShopMAD/src/shop/local/valueobjects/Mitarbeiter.java
@@ -2,4 +2,25 @@ package shop.local.valueobjects;
 
 public class Mitarbeiter {
 
+	private int mitarbeiterNr;
+	private String name;
+	
+	
+	public Mitarbeiter(int nr, String name) {
+		mitarbeiterNr = nr;
+		this.name = name;
+	}
+	
+	//Getter/ Setter/ usw Mitarbeiter-Eigenschaften
+	public int getMitarbeiterNr() {
+		return mitarbeiterNr;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
 }
