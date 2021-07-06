bce55d328cd486458a8ff44f6068b19d478a7638
dkanmaz <67149628+dkanmaz@users.noreply.github.com>
2021-07-06 12:54:37 +0200

    GUI Klassen und Ordner erstellt

--- Prog1_E-ShopMAD/src/shop/local/ui/gui/awt/ShopClientGUI.java
+++ Prog1_E-ShopMAD/src/shop/local/ui/gui/awt/ShopClientGUI.java
@@ -1,5 +1,13 @@
 package shop.local.ui.gui.awt;
 
-public class ShopClientGUI {
+import javax.swing.JFrame;
 
+import shop.local.ui.gui.panels.AnmeldenPanel;
+import shop.local.ui.gui.panels.AnmeldenPanel.AnmeldenListener;
+
+
+public class ShopClientGUI { //extends JFrame implements AnmeldenListene
+	
+	
+	private AnmeldenPanel anmeldenPanel;
 }
--- Prog1_E-ShopMAD/src/shop/local/ui/gui/panels/AnmeldenPanel.java
+++ Prog1_E-ShopMAD/src/shop/local/ui/gui/panels/AnmeldenPanel.java
@@ -1,5 +1,35 @@
 package shop.local.ui.gui.panels;
 
-public class AnmeldenPanel {
+import javax.swing.JPanel;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+//import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+//import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import shop.local.domain.Eshop;
+//import shop.local.ui.gui.awt.ShopClientGUI;
+//import shop.local.valueobjects.Kunde;
+
+
+public class AnmeldenPanel extends JPanel {
+	
+	public interface AnmeldenListener {
+		public void angemeldeterUser(int a);
+		public void angemeldeterMitarbeiter();
+		public void regMenue();
+
+	}
+	private AnmeldenListener anmeldenListener = null; 
 }
